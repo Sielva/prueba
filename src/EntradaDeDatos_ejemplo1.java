3b522e78a31a2950afa1ae044a7afa0c67277b2b
Sielva <jimenezsielva@hotmail.com>
2023-02-05 15:40:06 +0100

    Addedsout

--- src/EntradaDeDatos_ejemplo1.java
+++ src/EntradaDeDatos_ejemplo1.java
@@ -15,8 +15,8 @@ public class EntradaDeDatos_ejemplo1 {
 		int edad=entrada.nextInt();
 		
 		System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad+1) + " años.");
-		
-		
+
+		System.out.println("Vaya rollo de intellij");
 	}
 
 }
